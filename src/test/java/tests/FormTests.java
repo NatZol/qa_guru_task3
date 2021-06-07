9bfbbcbfc5f9742deec5271dce1453731e6c5606
NatZol <natalia.zlkh@gmail.com>
2021-06-07 21:29:04 +0300

    tests with page object and Java Faker

--- src/test/java/tests/FormTests.java
+++ src/test/java/tests/FormTests.java
@@ -1,16 +1,34 @@
 package tests;
 
+import com.github.javafaker.Faker;
+
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Condition.text;
+import pages.RegistrationPage;
 
 public class FormTests {
+
+    Faker faker = new Faker();
+
+    String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = faker.internet().emailAddress(),
+            gender = "Female",
+            phoneNumber = faker.phoneNumber().subscriberNumber(10),
+            month = "December",
+            year = "1990",
+            day = "23",
+            subject = "Computer Science",
+            hobby = "Reading",
+            pathname = "src/test/java/resources/1.jpeg",
+            picture = "1.jpeg",
+            address = faker.address().fullAddress(),
+            state = "NCR",
+            city = "Delhi";
+
+    RegistrationPage registrationPage = new RegistrationPage();
 
     @BeforeAll
     static void setUpConfig() {
@@ -20,37 +38,22 @@ public class FormTests {
 
     @Test
     void successSubmitFormTest() {
-        open("https://demoqa.com/automation-practice-form");
+        registrationPage.openPage();
 
-        $("#firstName").setValue("Natalia");
-        $("#lastName").setValue("Malakhov");
-        $("#userEmail").setValue("test@email.com");
-        $("input[value='Female']").parent().click();
-        $("#userNumber").setValue("1234567890");
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("December");
-        $(".react-datepicker__year-select").selectOption("1990");
-        $("div[aria-label='Choose Sunday, December 23rd, 1990']").click();
-        $("#subjectsInput").setValue("Comp").pressEnter();
-        $("#hobbies-checkbox-2").parent().click();
-        $("#uploadPicture").uploadFile(new File("src/test/java/resources/1.jpeg"));
-        $("textarea#currentAddress").setValue("Rishon leZion");
-        $("#state input").setValue("NCR").pressEnter();
-        $("#city input").setValue("Delhi").pressEnter();
+        registrationPage.typeFirstName(firstName);
+        registrationPage.typeLastName(lastName);
+        registrationPage.typeEmail(email);
+        registrationPage.chooseGender(gender);
+        registrationPage.typeUserNumber(phoneNumber);
+        registrationPage.chooseDateOfBirth(day, month, year);
+        registrationPage.chooseSubject(subject);
+        registrationPage.chooseHobby(hobby);
+        registrationPage.upLoadPicture(pathname);
+        registrationPage.typeAddress(address);
+        registrationPage.chooseState(state);
+        registrationPage.chooseCity(city);
+        registrationPage.submitForm();
 
-        $("#submit").click();
-
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text("Label"), text("Values"));
-        $(".table-responsive").shouldHave(text("Student Name"), text("Natalia Malakhov"));
-        $(".table-responsive").shouldHave(text("Student Email"), text("test@email.com"));
-        $(".table-responsive").shouldHave(text("Gender"), text("Female"));
-        $(".table-responsive").shouldHave(text("Mobile"), text("1234567890"));
-        $(".table-responsive").shouldHave(text("Date of Birth"), text("23 December,1990"));
-        $(".table-responsive").shouldHave(text("Subjects"), text("Computer Science"));
-        $(".table-responsive").shouldHave(text("Hobbies"), text("Reading"));
-        $(".table-responsive").shouldHave(text("Picture"), text("1.jpeg"));
-        $(".table-responsive").shouldHave(text("Address"), text("Rishon leZion"));
-        $(".table-responsive").shouldHave(text("State and City"), text("NCR Delhi"));
+        registrationPage.checkRegistrationResults(firstName, lastName, email, gender, phoneNumber, day, month, year, subject, hobby, picture, address, state, city);
     }
 }
